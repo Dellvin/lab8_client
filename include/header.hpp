f46bd5c3fcbcd5504c5a1818963c96d2fe487ee8
Dellvin <dellvin.black@gmail.com>
2020-03-14 11:51:20 +0300

    make class for client

--- include/header.hpp
+++ include/header.hpp
@@ -10,54 +10,75 @@
 #include <chrono>
 #include <thread>
 #include <unistd.h>
-#include <string>
 
 #define BOOST_ASIO_SEPARATE_COMPILATION
 
-using boost::posix_time::ptime;
 
-void connection(const boost::system::error_code &ec);
+class Client {
+public:
+    Client() : ep(boost::asio::ip::address::
+                  from_string("127.0.0.1"), 1024), sock(service) {
+        sock.connect(ep);
+        getLogin();
 
-void pinging(boost::asio::ip::tcp::socket *sock) {
-    while (true) {
-        sleep(std::rand() % 8);
-        try {
-            sock->write_some(boost::asio::buffer("ping\n"));
-        } catch (...) {
-            return;
+
+        mainLoop();
+    }
+
+private:
+    void mainLoop() {
+        std::thread pingThread(pinging, &sock, &ep);
+        while (true) {
+            std::string s;
+            std::cout << "entering: ";
+            std::cin >> s;
+            s.push_back('\n');
+            sock.write_some(boost::asio::buffer(s));
+            sock.read_some(boost::asio::buffer(s));
+            if (s == "bye\n") {
+                sock.close();
+                std::cout << "GOOD BYE!" << std::endl;
+                return;
+            }
+            std::cout << "answer: " << s << std::endl;
         }
     }
-}
 
+    static void pinging(boost::asio::ip::tcp::socket *sock,
+                        boost::asio::ip::tcp::endpoint *ep) {
+        const std::string PING = "ping\n";
+        while (true) {
+            sleep(std::rand() % 8);
+            try {
+                sock->write_some(boost::asio::buffer(PING));
+            } catch (...) {
+                return;
+            }
+        }
+    }
 
-int main() {
+    void getLogin() {
+        std::string answer;
+        while (answer != LOGIN_OK) {
+            std::cout << "Your login is: " << login << std::endl;
+            login.push_back('\n');
+            sock.write_some(boost::asio::buffer(login));
+            char c[32];
+            sock.read_some(boost::asio::buffer(c));
+            answer = c;
+            std::cout << "answer: '" << answer << "'" << std::endl;
+        }
+    }
+
+public:
+    std::string login = "Dellvin";
+
+private:
     boost::asio::io_service service;
-    boost::asio::ip::tcp::endpoint ep(boost::asio::ip::address::
-    from_string("127.0.0.1"), 1024);
-    boost::asio::ip::tcp::socket sock(service);
-    sock.connect(ep);
-    sock.write_some(boost::asio::buffer("Alice\n"));
-    char c[32];
-    sock.read_some(boost::asio::buffer(c));
-    std::cout << c;
-    std::thread pingThread(pinging, &sock);
-    while (true) {
-        memset(c, '\0', 32);
-        std::string s;
-        std::cout << "entering: ";
-        std::cin >> s;
-        s.push_back('\n');
-        sock.write_some(boost::asio::buffer(s));
-        sock.read_some(boost::asio::buffer(s));
-        if (s == "bye\n") {
-            sock.close();
-            std::cout << "GOOD BYE!" << std::endl;
-            break;
-        }
-        std::cout << "answer: " << s << std::endl;
-    }
-    return 0;
-}
+    boost::asio::ip::tcp::endpoint ep;
+    boost::asio::ip::tcp::socket sock;
+    const std::string LOGIN_OK = "login_ok\n";
+};
 
 
 #endif // INCLUDE_HEADER_HPP_
